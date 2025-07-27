f74a8c19d3e919b444c46e773804f11a6f0eb58c
Oguzhan Savas <oguzhansavas359@gmail.com>
2025-07-27 15:15:35 +0200

    reqs and .yaml updated

--- app/main.py
+++ app/main.py
@@ -128,4 +128,5 @@ def forecast_irradiance(
     }
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
